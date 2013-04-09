c12a470dea958958257508c5b0e09233da8b1a6b
Marcin Nowak <Marcin.Nowak@sonyx.net>
2013-04-09 21:38:04 +0200

    mysql persistence of bodies now operational

--- core/append.php
+++ core/append.php
@@ -3,6 +3,12 @@
 if (empty($_POST)) exit;
 require_once '../config.php';
 
+$fields  = "body";
+$values  = "COMPRESS('".  $_POST['body']  ."')";
+
+$body_id = db_insert("smt2_bodies",$fields, $values);
+echo $body_id;
+
 $values  = "sess_time = '".                         (float) $_POST['time']    ."',";
 $values .= "vp_width  = '".                         (int)   $_POST['pagew']   ."',";
 $values .= "vp_height = '".                         (int)   $_POST['pageh']   ."',";
@@ -10,7 +16,8 @@ $values .= "coords_x  = CONCAT(COALESCE(coords_x, ''), ',". $_POST['xcoords'] ."
 $values .= "coords_y  = CONCAT(COALESCE(coords_y, ''), ',". $_POST['ycoords'] ."'),";
 $values .= "clicks    = CONCAT(COALESCE(clicks,   ''), ',". $_POST['clicks']  ."'),";
 $values .= "hovered   = CONCAT(COALESCE(hovered,  ''), ',". array_sanitize($_POST['elhovered']) ."'),";
-$values .= "clicked   = CONCAT(COALESCE(clicked,  ''), ',". array_sanitize($_POST['elclicked']) ."')";
+$values .= "clicked   = CONCAT(COALESCE(clicked,  ''), ',". array_sanitize($_POST['elclicked']) ."'),";
+$values .= "bodies    = CONCAT(COALESCE(bodies,   ''), ',". strval($body_id)  ."')";
 
 db_update(TBL_PREFIX.TBL_RECORDS, $values, "id='".$_POST['uid']."'");
 ?>
